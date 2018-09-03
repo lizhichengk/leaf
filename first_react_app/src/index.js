788558e4b9d1baf447d1ba64415a65f64954c7e8
lizicheng <lizicheng@weshare.com.cn>
2018-09-03 20:26:22 +0800

    LF-0005: add router

--- first_react_app/src/index.js
+++ first_react_app/src/index.js
@@ -7,9 +7,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {BrowserRouter,Route,Link} from 'react-router-dom';
+import Home from './view/Home';
+import About from './view/About';
+import NotFound from './view/NotFound';
+
 ReactDOM.render(
     <Provider store={store}>
-        <ControlPanel />
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li> <link to='/'></link></li>
+                    <li> <link to='/about'></link></li>
+                    <li> <link to='/notFound'></link></li>
+                </ul>
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/about' component={About}></Route>
+                <Route path='/notFound' component={NotFound}></Route>
+            </div>
+        </BrowserRouter>
     </Provider>
     ,document.getElementById('root')
     );
