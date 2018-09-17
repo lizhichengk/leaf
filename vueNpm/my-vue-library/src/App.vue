c830665576cd7be3f099fca69a231924a45a6b03
lizhicheng <lizhicheng@people.cn>
2018-09-17 15:25:44 +0800

    LF-0007: add npm

---
+++ vueNpm/my-vue-library/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Banner>Fundebug：最专业的应用错误监控平台!</Banner>
+    <Li left-text ='姓名' right-text = '张三'>
+
+    </Li>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Banner from './components/Banner.vue'
+import Li from './components/Li.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Banner,
+    Li
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueNpm/my-vue-library/src/components/Li.vue
@@ -0,0 +1,46 @@
+<template>
+    <li :style = style>
+        <span>{{leftText}}</span>
+        <span>{{rightText}}</span>
+    </li>
+</template>
+<script>
+const defaultStyle = {
+    height:"45px",
+    lineHeight:"45px",
+}
+export default {
+    props:{
+        leftText:{
+            type:String,
+            default:'左侧text'
+        },
+        rightText:{
+            type:String,
+            default:'右侧text'
+        }
+    },
+    data(){
+        return {
+            style:{
+                ...defaultStyle
+            }
+        }
+    }    
+}
+</script>
+<style>
+    li{
+        list-style: none;
+        font-size: 14px;
+        clear: both;
+    }
+    li span:nth-of-type(1){
+        float: left;
+    }
+    li span:nth-of-type(2){
+        float: right;
+    }
+</style>
+
+
